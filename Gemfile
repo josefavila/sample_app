2e8df854ff3545ca4bbedfc25fc83fb5e220c3d5
Jose Favila <jose.favilad@gmail.com>
2013-04-10 18:14:58 -0500

    PGsql

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.6'
+gem 'pg'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
@@ -26,10 +27,9 @@ group :test do
   gem 'capybara', '1.1.2'
 end
 
-group :production do
-	gem 'pg', '0.12.2'
-end
-
+#group :production do
+#	gem 'pg', '0.12.2'
+#end
 
 gem 'jquery-rails'
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,20 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+  describe "Contact Page" do
+    it "should have the content 'Sample App'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: 'Contact')
+    end
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', 
+                            text: "#{base_title} | Contact")
+    end
+  end
 
   describe "Home Page" do
     it "should have the content 'Sample App'" do
@@ -11,7 +25,7 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/home'
       page.should have_selector('title', 
-                            text: "Ruby on Rails Tutorial Sample App | Home")
+                            text: "#{base_title} | Home")
     end
   end
 
@@ -25,7 +39,7 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/help'
       page.should have_selector('title', 
-                            text: "Ruby on Rails Tutorial Sample App | Help")
+                            text: "#{base_title} | Help")
     end
   end
 
@@ -39,7 +53,7 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title', 
-                        text: "Ruby on Rails Tutorial Sample App | About Us")
+                        text: "#{base_title} | About Us")
     end
   end
 
